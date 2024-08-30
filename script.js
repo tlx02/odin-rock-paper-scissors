d456bac85fed8caf5ca72855ce73347353a2aa08
tlx02 <e0774379@u.nus.edu>
2024-08-30 13:47:14 +0800

    Write function to play a round of the game

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     value = Math.random();
@@ -16,3 +17,41 @@ function getHumanChoice() {
     value = prompt("What's your choice? (rock / paper / scissors)");
     return value.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!"); 
+        return;
+    }
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock"); 
+            computerScore++;
+        } else {
+            console.log("You win! Rock beats Scissors"); 
+            humanScore++;
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beats Paper"); 
+            computerScore++;
+        } else {
+            console.log("You win! Paper beats Rock"); 
+            humanScore++;
+        }
+    } else {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors"); 
+            computerScore++;
+        } else {
+            console.log("You win! Scissors beats Paper"); 
+            humanScore++;
+        }
+    }
+  }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
+  
